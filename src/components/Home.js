f6040677ded41679437405e97c825482875387d0
cherrysctse <133928214+cherrysctse@users.noreply.github.com>
2023-07-18 16:38:33 -0700

    updated products.js and images on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,14 @@ class Home extends Component {
     render() {
         return (
             <div className="main-content">
-                <img src="./images/index_image.jpg" id="mainImg" alt="camping" width="500" /> 
-                <hr/>
+                <img src="./images/index_image.jpg" id="mainImg" alt="camping" width="500" />
+                <hr />
                 <h2 className="subtitle">Everything you need for camping</h2>
                 <div className="table">
                     <div className="row">
-                        <span><img src="./images/index_image.jpg" alt="Tents" width="150" /></span>
-                        <span><img src="./images/index_image.jpg" alt="Sleeping Bags" width="150" /></span>
-                        <span><img src="./images/index_image.jpg" alt="Cooking Utensils" width="150" /></span>
+                        <span><img src="./images/Tents_index.jpg" alt="Tents" width="150" height="150" /></span>
+                        <span><img src="./images/sleeping-bags_index.jpg" alt="Sleeping Bags" width="150" height="150" /></span>
+                        <span><img src="./images/cooking_utensils_index.jpg" alt="Cooking Utensils" width="150" height="150" /></span>
                     </div>
                     <div className="row">
                         <span>Tents</span>
