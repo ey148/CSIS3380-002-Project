838636a1dca19b36c39050a776cd34fc14542846
cherrysctse <133928214+cherrysctse@users.noreply.github.com>
2023-08-05 22:58:01 -0700

    updated rating function

--- src/components/ProductBox.js
+++ src/components/ProductBox.js
@@ -1,48 +1,48 @@
 import React, { useState } from 'react';
 
 const ProductBox = (props) => {
-    const [inputRating, setInputRating] = useState('');
+  const [inputRating, setInputRating] = useState('');
 
-    const handleInputRatingChange = (event) => {
-        setInputRating(event.target.value);
-    };
+  const handleInputRatingChange = (event) => {
+    setInputRating(event.target.value);
+  };
 
-    const handleSubmitRating = () => {
-        if (inputRating !== '') {
-            const newCountRating = props.countRating + 1;
-            const newRating =
-                (props.rating * props.countRating + parseFloat(inputRating)) / newCountRating;
-            props.updateRating(props.productData.productId, newRating, newCountRating);
-            setInputRating('');
-        }
-    };
+  const handleSubmitRating = () => {
+    if (inputRating !== '') {
+      const newCountRating = props.countRating + 1;
+      const newRating =
+        (props.rating * props.countRating + parseFloat(inputRating)) / newCountRating;
+      props.updateRating(props.productData.productId, newRating, newCountRating);
+      setInputRating('');
+    }
+  };
 
-    return (
-        <div className="box">
-            <img className="detailImg" src={props.img} alt="product" />
-            <div>
-                <h5>{props.title}</h5>
-                <ul>
-                    <li>{props.brand}</li>
-                    <li>CAD {props.price}</li>
-                    <li>
-                        Input Rating Score(1-5):
-                        <input
-                            type="text"
-                            id="inputRating"
-                            size="5"
-                            value={inputRating}
-                            onChange={handleInputRatingChange}
-                        />
-                        <input type="button" id="BtnInputRating" value="Submit" onClick={handleSubmitRating} />
-                    </li>
-                    <li>Average Ratings: {props.rating}</li>
-                    <li>Rating Scores inputted: {props.countRating}</li>
-                    <h4 onClick={() => props.selectedProduct(props.productData.productId)}>show product details</h4>
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div className="box">
+      <img className="detailImg" src={props.img} alt="product" />
+      <div>
+        <h5>{props.title}</h5>
+        <ul>
+          <li>{props.brand}</li>
+          <li>CAD {props.price}</li>
+          <li>
+            Input Rating Score:
+            <input
+              type="text"
+              id="inputRating"
+              size="5"
+              value={inputRating}
+              onChange={handleInputRatingChange}
+            />
+            <input type="button" id="BtnInputRating" value="Submit" onClick={handleSubmitRating} />
+          </li>
+          <li>Average Ratings: {props.rating}</li>
+          <li>Rating Scores inputted: {props.countRating}</li>
+          <h4 onClick={() => props.selectedProduct(props.productData._id)}>show product details</h4>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default ProductBox;
