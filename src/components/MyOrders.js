2a6c6761657598b98305f3ee82b96003468dc214
k-lili <lhlkarli@gmail.com>
2023-08-12 12:24:14 -0700

    karli - 12 aug ordertable component

--- src/components/MyOrders.js
+++ src/components/MyOrders.js
@@ -3,6 +3,49 @@ import axios from 'axios';
 // import 'bootstrap-icons/font/bootstrap-icons.css';
 import apiLink from './config.js'
 
+const OrderTable = (props) => {
+
+    function formatDate(dateString) {
+        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+        return new Date(dateString).toLocaleString(undefined, options);
+    }
+
+    return (
+        <div key={props.index+1} className="list-table">
+            <p>Order number: {props.order._id}<br/>Order date: {formatDate(props.order.createdAt)}</p>
+
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        {/* <th scope="col"></th> */}
+                        <th scope="col" id="col4">Item(s)</th>
+                        <th scope="col" id="col5" className="itemQuantity">Quantity</th>
+                        <th scope="col" id="col6" className="itemPrice">Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {(props.order.items).map((item, index) => 
+
+                        <tr scope="row" key={index+1}>
+                            {/* <td><img src={item.productImg} alt="" width="100" height="100"/></td>     */}
+                            <td>{item.productTitle}</td>
+                            <td className="itemQuantity">{item.quantity}</td>
+                            <td className="itemPrice">${(item.priceSubTotal).toFixed(2)}</td>
+                        </tr>
+                    )}
+                        <tr scope="row">
+                            {/* <td></td>*/}
+                            <td colSpan="2"><strong>Grand total:</strong></td>
+                            <td className="itemPrice"><strong>${props.order.grandTotal}</strong></td>
+                        </tr>
+                </tbody>
+
+            </table>
+        </div>
+    )
+};
+
+
 const MyOrders = () => {
 
     const [userId] = useState(localStorage.getItem('userId'));
@@ -13,7 +56,6 @@ const MyOrders = () => {
 
         axios
             .get(`${apiLink}/order?userId=${userId}`) 
-            // .get(`http://localhost:5000/order?userId=${userId}`)
             .then((response) => {
                 setOrderList(response.data);
                 console.log(response.data);
@@ -24,10 +66,7 @@ const MyOrders = () => {
             // eslint-disable-next-line
     },[]);
 
-    function formatDate(dateString) {
-        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
-        return new Date(dateString).toLocaleString(undefined, options);
-      }
+    
 
     return (
         <div className="main-content">
@@ -40,44 +79,13 @@ const MyOrders = () => {
                     <h4 className="subHeader">No. of outstanding orders: {orderList.length}</h4>
 
                     {orderList.map((order, index) => {
-                            return (
-                                <div key={index+1} className="list-table">
-                                    <p>Order number: {order._id}<br/>Order date: {formatDate(order.createdAt)}</p>
-        
-                                    <table className="table table-hover">
-                                        <thead>
-                                            <tr>
-                                                {/* <th scope="col"></th> */}
-                                                <th scope="col" id="col4">Item(s)</th>
-                                                <th scope="col" id="col5" className="itemQuantity">Quantity</th>
-                                                <th scope="col" id="col6" className="itemPrice">Price</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {(order.items).map((item, index) => 
-        
-                                                <tr scope="row" key={index+1}>
-                                                    {/* <td><img src={item.productImg} alt="" width="100" height="100"/></td>     */}
-                                                    <td>{item.productTitle}</td>
-                                                    <td className="itemQuantity">{item.quantity}</td>
-                                                    <td className="itemPrice">${(item.priceSubTotal).toFixed(2)}</td>
-                                                </tr>
-                                            )}
-                                                <tr scope="row">
-                                                    {/* <td></td>*/}
-                                                    <td colspan="2"><strong>Grand total:</strong></td>
-                                                    <td className="itemPrice"><strong>${order.grandTotal}</strong></td>
-                                                </tr>
-                                        </tbody>
-        
-                                    </table>
-                                </div>
-                            )
-                    }
-                        
-                        
-                        
-                    )}
+                        return(
+                            <OrderTable 
+                            order={order}
+                            index={index}
+                        /> 
+                        )                              
+                    })}
                     
                 </div>
             }
